22f6a2af04eafd7615baece0c8934ea441cc8e76
liuhui <liuhuidut@sina.com>
2020-12-03 10:33:30 +0800

    results表group_results字段数据迁移增加

--- database/migrations/2020_12_02_172212_add_group_results_to_results_table.php
+++ database/migrations/2020_12_02_172212_add_group_results_to_results_table.php
@@ -14,7 +14,8 @@ class AddGroupResultsToResultsTable extends Migration
     public function up()
     {
         Schema::table('results', function (Blueprint $table) {
-            //
+            //ALTER TABLE `results` ADD `group_results` JSON NOT NULL AFTER `results`;
+            $table->json('group_results', 255)->comment('分组结果');
         });
     }
 
@@ -26,7 +27,7 @@ class AddGroupResultsToResultsTable extends Migration
     public function down()
     {
         Schema::table('results', function (Blueprint $table) {
-            //
+            $table->dropColumn('group_results');
         });
     }
 }
